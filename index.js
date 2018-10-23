84334563d0d7cbfcc4915d23bd015b67ef1ac4cf
Adam Duff <j.adam.duff@gmail.com>
2018-10-23 22:55:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Code your solutions in this file
+
+function printBadges(array) {
+  for (let i = 1; i <= array.length; i++) {
+    console.log("Welcome " + array[i - 1] + "! You are employee #" + i + ".")
+  }
+}
