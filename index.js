d9c6e920faf65f0f93d9dffab3849733c88f1d9f
Adam Duff <j.adam.duff@gmail.com>
2018-10-23 23:02:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,21 @@
 
 function printBadges(array) {
   for (let i = 1; i <= array.length; i++) {
-    console.log("Welcome " + array[i - 1] + "! You are employee #" + i + ".")
+    console.log("Welcome " + array[i - 1] + "! You are employee #" + i + ".");
   }
-  return array
+  return array;
+}
+
+function tailsNeverFails() {
+  let flip = 'tails';
+  let tailsCounter = 0;
+  while (flip === 'tails') {
+    let flipper = Math.random();
+    if (flipper >= 0.5) {
+      flip = 'heads';
+    } else {
+      tailsCounter++;
+    }
+  }
+  return tailsCounter;
 }
